121f6233eadfb8346fa5386a36eca92322a0e6cb
Abins26 <abinsms26@gmail.com>
2024-05-14 09:53:19 +0530

    new 2.001

--- src/components/Studio/Studio.jsx
+++ src/components/Studio/Studio.jsx
@@ -1,10 +1,11 @@
 import React,{useState} from 'react'
 import disney from "../../images/disney.png"
 import marvel from  "../../images/marvel.png"
-import starwar from "../../images/starwar.jpg"
+// import starwar from "../../images/starwar.jpg"
 import starwar2 from "../../images/imagesstarwar.png"
 import pixar from "../../images/pixar.jpeg"
 import ngo from "../../images/ngo.png"
+import spl from "../../images/hotsatarspl.png"
 
 const cardsData = [
   { title: 'Card 1', videoUrl:" https://img10.hotstar.com/video/upload/sources/r1/cms/animations/qh3yh_1587393133132",imageUrl: disney },
@@ -12,7 +13,7 @@ const cardsData = [
   { title: 'Card 3', videoUrl:" https://img10.hotstar.com/video/upload/sources/r1/cms/animations/1on5cf_1587393232549", imageUrl: marvel },
   { title: 'Card 4', videoUrl:" https://img10.hotstar.com/video/upload/sources/r1/cms/animations/utwmfd_1587393376512", imageUrl: starwar2 ,width:"204px",height:'100px' },
   { title: 'Card 5', videoUrl:" https://img10.hotstar.com/video/upload/sources/r1/cms/animations/ojerhm_1587393280208", imageUrl: ngo },
-  { title: 'Card 6', videoUrl:" https://img10.hotstar.com/video/upload/sources/r1/cms/animations/qc9clm_1669284974594", imageUrl: 'image5.jpg' },
+  { title: 'Card 6', videoUrl:" https://img10.hotstar.com/video/upload/sources/r1/cms/animations/qc9clm_1669284974594", imageUrl: spl },
 
 ];
 console.log(cardsData);
@@ -22,7 +23,7 @@ function Studio() {
   return (
         <div>
             <div>
-                <div className="flex flex-wrap pt-10 pl-40 pr-24 h-56 bg-red-600">
+                <div className="flex flex-wrap pt-10 pl-40 pr-24 h-56 bg-black">
                     {cardsData.map((card, index) => (
                         <div key={index} onMouseEnter={() => setHoveredIndex(index)}
                         onMouseLeave={() => setHoveredIndex(null)} className="w-1/6 p-2">
